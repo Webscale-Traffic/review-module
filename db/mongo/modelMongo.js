264a28dceba690836970b1b16f74b2f1ad293cbe
maureenmlee <maureen.ml.lee@gmail.com>
2020-09-16 13:05:02 -0700

    Cleaned up code more

--- db/mongo/modelMongo.js
+++ db/mongo/modelMongo.js
@@ -1,4 +1,4 @@
-const db = require('../index.js');
+const db = require('../index.gitjs');
 const Reviews = require('./mongo.js');
 
 const getReviews = (roomID, callback) => {
--- db/mongo/mongo.js
+++ db/mongo/mongo.js
@@ -1,3 +1,38 @@
+// const { MongoClient } = require("mongodb");
+// const
+// // Connection URI
+// const uri =
+//   "mongodb+srv://sample-hostname:27017/?poolSize=20&w=majority?compressors=zstd,snappy,zlib";
+
+// // Create a new MongoClient
+// const client = new MongoClient(uri);
+
+// async function run() {
+//   try {
+//     // Connect the client to the server
+//     await client.connect();
+
+//     // Establish and verify connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Connected successfully to server");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/reviews', { useNewUrlParser: true });
